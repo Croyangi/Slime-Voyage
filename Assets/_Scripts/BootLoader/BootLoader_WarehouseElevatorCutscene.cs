f3ae6349f3ffcc9a57551d6aeae443a9d8de0494
Croyangi <hellomuffinlin@gmail.com>
2024-05-18 00:30:49 -0400

    Elevator Cutscene more

--- Assets/_Scripts/BootLoader/BootLoader_WarehouseElevatorCutscene.cs
+++ Assets/_Scripts/BootLoader/BootLoader_WarehouseElevatorCutscene.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
 {
-    [Header("References")]
+    [Header("Elevator Parts")]
     [SerializeField] private GameObject elevator;
     [SerializeField] private float elevatorRiseYPos;
     [SerializeField] private float elevatorRiseTime;
@@ -23,13 +25,26 @@ public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
     [SerializeField] private GameObject gear1;
     [SerializeField] private GameObject gear2;
 
-    [SerializeField] private SpriteRenderer elevatorLightSr;
-    [SerializeField] private Light2D elevatorLight;
+    [SerializeField] private GameObject slime;
 
+    [Header("Elevator Light")]
+    [SerializeField] private GameObject elevatorLight;
+    [SerializeField] private SpriteRenderer elevatorLightSr;
+    [SerializeField] private Light2D elevatorLight2D;
+    [SerializeField] private Sprite elevatorLightOn;
+    [SerializeField] private Sprite elevatorLightOff;
+
+    [SerializeField] private GameObject elevatorRedLight;
+    [SerializeField] private GameObject elevatorRotatingRedLights;
+    [SerializeField] private Sprite elevatorRedLightOn;
+
+    [Header("References")]
     [SerializeField] private AudioClip music_elevatorCutscene;
 
     [SerializeField] private Image fadeOutTransition;
     [SerializeField] private float elapsedTime;
+
+    [SerializeField] private GameObject fallingParticles;
 
     [Header("Scene")]
     [SerializeField] private SceneQueue _sceneQueue;
@@ -87,7 +102,6 @@ public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
 
     private IEnumerator FadeOutTransition()
     {
-        Debug.Log("logging");
         elapsedTime += Time.deltaTime;
 
         float alpha = Mathf.Lerp(1f, 0f, elapsedTime / 6f);
@@ -95,11 +109,10 @@ public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
         fadeOutTransition.color = new Color(fadeOutTransition.color.r, fadeOutTransition.color.g, fadeOutTransition.color.b, alpha);
 
         yield return new WaitForFixedUpdate();
-        StartCoroutine(FadeOutTransition());
-        /*if (fadeOutTransition.color.a > alpha)
+        if (fadeOutTransition.color.a > 0f)
         {
             StartCoroutine(FadeOutTransition());
-        }*/
+        }
     }
 
     private IEnumerator InitiateElevatorCutscene()
@@ -116,6 +129,8 @@ public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
         yield return new WaitForSeconds(elevatorRiseTime);
 
         // Sudden Stop
+        StartCoroutine(FlickerLight());
+
         LeanTween.moveLocalY(elevator, elevator.transform.position.y - 0.5f, 1f).setEaseInBounce();
         LeanTween.cancel(pulley);
         LeanTween.cancel(gear1);
@@ -130,6 +145,14 @@ public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
         yield return new WaitForSeconds(elevatorSlowFallTime);
 
         // Fall
+        fallingParticles.SetActive(true);
+
+        InitiateRedLights();
+
+        slime.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+        slime.GetComponent<Rigidbody2D>().gravityScale = 0;
+        StartCoroutine(ApplyFallingVelocity());
+
         LeanTween.moveY(elevator, elevator.transform.position.y - elevatorFallYPos, elevatorFallTime);
 
         LeanTween.cancel(pulley);
@@ -140,8 +163,49 @@ public class BootLoader_WarehouseElevatorCutscene : MonoBehaviour
         LeanTween.rotateAround(gear2, Vector3.forward, -360, 0.1f).setLoopClamp();
     }
 
-    private IEnumerator ElevatorLightFlicker()
+    private void InitiateRedLights()
     {
-        yield return null;
+        elevatorLight.SetActive(false);
+        elevatorRedLight.SetActive(true);
+        LeanTween.rotateAround(elevatorRotatingRedLights, Vector3.up, 360, 2f).setLoopClamp();
+    }
+
+    private IEnumerator FlickerLight()
+    {
+        ToggleLight(false);
+        yield return new WaitForSeconds(0.3f);
+        ToggleLight(true);
+        yield return new WaitForSeconds(0.3f);
+
+        for (int i = 0; i < 3; i++)
+        {
+            ToggleLight(false);
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+            ToggleLight(true);
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+        }
+
+        yield return new WaitForSeconds(0.1f);
+        ToggleLight(false);
+    }
+
+    private void ToggleLight(bool state)
+    {
+        elevatorLight2D.gameObject.SetActive(state);
+
+        if (state)
+        {
+            elevatorLightSr.sprite = elevatorLightOn;
+        } else
+        {
+            elevatorLightSr.sprite = elevatorLightOff;
+        }
+    }
+
+    private IEnumerator ApplyFallingVelocity()
+    {
+        slime.GetComponent<Rigidbody2D>().velocity = Vector2.up * 10f;
+        yield return new WaitForFixedUpdate();
+        StartCoroutine(ApplyFallingVelocity());
     }
 }
